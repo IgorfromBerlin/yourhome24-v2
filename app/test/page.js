5cf2fe33c73a9c4f93b6d7ee6f476b0cf23784ad
Georg Vostrikov <vostrikov@Georgs-MacBook-Air.local>
2025-10-20 18:10:24 +0300

    init v2 with /test route

---
+++ app/test/page.js
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>/test ok ✅</h1>
+      <p>Wenn du das hier siehst, funktioniert dein Routing 🎉</p>
+    </main>
+  );
+}
